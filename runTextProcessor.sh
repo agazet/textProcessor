6c80bf6920299e145723ab6dd365822f4454cd32
aga <aga@zabtop>
2017-10-14 21:32:07 -0700

    update run script

--- runTextProcessor.sh
+++ runTextProcessor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ping www.google.com >> /tmp/pingGoogle.log | tail -f /tmp/pingGoogle.log | node textProcessor/runTextProcessor.js
+ping www.google.com >> /tmp/pingGoogle.log | tail -f /tmp/pingGoogle.log | node textProcessor/runTextProcessor.js log log/textProcessor.log
